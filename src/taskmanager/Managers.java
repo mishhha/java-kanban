06dc05532639200dd9d4c3535cb61303f9b31af3
Misha Plehanov <mih.plekhanov@yandex.ru>
2025-08-06 01:34:29 +0300

    Исправить CodeStyle на 9  строке Ma, после авто теста GitHub

--- src/taskmanager/Managers.java
+++ src/taskmanager/Managers.java
@@ -6,7 +6,7 @@ public class Managers {
         return new InMemoryTaskManager(getDefaultHistory());
     }
 
-    public static HistoryManager getDefaultHistory(){
+    public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
     }
 
