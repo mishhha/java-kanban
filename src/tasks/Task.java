856be23bd00736bb3d6958ea6704bd4b22314b01
Misha Plehanov <mih.plekhanov@yandex.ru>
2025-10-07 11:58:08 +0300

    заменить сортировку лямбда на современный Comparator

--- src/tasks/Task.java
+++ src/tasks/Task.java
@@ -17,36 +17,9 @@ public class Task {
     private Duration duration;
     private LocalDateTime startTime;
 
-    public static final Comparator<Task> BY_START_TIME = (o1, o2) -> {
-        // 1. Если startTime null
-        if (o1.getStartTime() == null && o2.getStartTime() == null) {
-            return 0;
-        }
-        if (o1.getStartTime() == null) {
-            return 1;
-        }
-        if (o2.getStartTime() == null) {
-            return -1;
-        }
-        // 2. Если время есть
-        LocalDateTime start1 = o1.getStartTime();
-        LocalDateTime start2 = o2.getStartTime();
-
-        if (start1.isBefore(start2)) {
-            return -1;
-        }
-        if (start1.isAfter(start2)) {
-            return 1;
-        }
-        // 3. Если время одинаково, проверим по Id
-        if (o1.getId() < o2.getId()) {
-            return -1;
-        }
-        if (o1.getId() > o2.getId()) {
-            return 1;
-        }
-        return 0;
-    };
+    public static final Comparator<Task> BY_START_TIME = Comparator
+        .comparing(Task::getStartTime, Comparator.nullsLast(Comparator.naturalOrder()))
+        .thenComparing(Task::getId);
 
     public Duration getDuration() {
         return duration;
