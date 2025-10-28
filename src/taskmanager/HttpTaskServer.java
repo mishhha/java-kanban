17fcc0bd3b811075e1d321edd67fb0d4da1d8d04
Misha Plehanov <mih.plekhanov@yandex.ru>
2025-10-28 23:16:32 +0300

    добавить в сервер, работу с менеджером задач, дать доступ обработчикам

--- src/taskmanager/HttpTaskServer.java
+++ src/taskmanager/HttpTaskServer.java
@@ -10,22 +10,32 @@ import java.net.InetSocketAddress;
 import java.io.IOException;
 
 import com.google.gson.Gson;
+import tasks.Task;
 
 public class HttpTaskServer {
+
     public static void main(String[] args) throws IOException {
+
         HttpServer httpServer = HttpServer.create(); // Создали Http сервер
         httpServer.bind(new InetSocketAddress(8080),0); // Слушаем порт
+        httpServer.start();
 
-        httpServer.createContext("/tasks", new TaskHandler());
-        httpServer.createContext("/subtasks", new SubTaskHandler());
-        httpServer.createContext("/epics", new EpicHandler());
-        httpServer.createContext("/history", new HistoryHandler());
-        httpServer.createContext("/prioritized", new PrioritizedHandler());
+        TaskManager manager = Managers.getDefault();
 
+        httpServer.createContext("/tasks", new TaskHandler(manager));
+        httpServer.createContext("/subtasks", new SubTaskHandler(manager));
+        httpServer.createContext("/epics", new EpicHandler(manager));
+        httpServer.createContext("/history", new HistoryHandler(manager));
+        httpServer.createContext("/prioritized", new PrioritizedHandler(manager));
 
     }
 
     static class TaskHandler implements HttpHandler {
+        TaskManager taskManager;
+
+        public TaskHandler(TaskManager taskManager) {
+            this.taskManager = taskManager;
+        }
 
         @Override
         public void handle(HttpExchange exchange) throws IOException {
@@ -35,6 +45,11 @@ public class HttpTaskServer {
     }
 
     static class SubTaskHandler implements HttpHandler {
+        TaskManager taskManager;
+
+        public SubTaskHandler(TaskManager taskManager) {
+            this.taskManager = taskManager;
+        }
 
         @Override
         public void handle(HttpExchange exchange) throws IOException {
@@ -44,6 +59,11 @@ public class HttpTaskServer {
     }
 
     static class EpicHandler implements HttpHandler {
+        TaskManager taskManager;
+
+        public EpicHandler(TaskManager taskManager) {
+            this.taskManager = taskManager;
+        }
 
         @Override
         public void handle(HttpExchange exchange) throws IOException {
@@ -53,6 +73,11 @@ public class HttpTaskServer {
     }
 
     static class HistoryHandler implements HttpHandler {
+        TaskManager taskManager;
+
+        public HistoryHandler(TaskManager taskManager) {
+            this.taskManager = taskManager;
+        }
 
         @Override
         public void handle(HttpExchange exchange) throws IOException {
@@ -62,13 +87,16 @@ public class HttpTaskServer {
     }
 
     static class PrioritizedHandler implements HttpHandler {
+        TaskManager taskManager;
 
+        public PrioritizedHandler(TaskManager taskManager) {
+            this.taskManager = taskManager;
+        }
         @Override
         public void handle(HttpExchange exchange) throws IOException {
 
         }
 
     }
-
 
 }
