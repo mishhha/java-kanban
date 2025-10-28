54f5b99b2dc8b5697660776e5483e85e0d08e044
Misha Plehanov <mih.plekhanov@yandex.ru>
2025-10-28 22:21:01 +0300

    Создать обработчики для 5 базовых путей

--- src/taskmanager/HttpTaskServer.java
+++ src/taskmanager/HttpTaskServer.java
@@ -1,7 +1,10 @@
 package taskmanager;
 
+import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
 import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
 
@@ -10,5 +13,59 @@ public class HttpTaskServer {
         HttpServer httpServer = HttpServer.create(); // Создали Http сервер
         httpServer.bind(new InetSocketAddress(8080),0); // Слушаем порт
 
+        httpServer.createContext("/tasks", new TaskHandler());
+        httpServer.createContext("/subtasks", new SubTaskHandler());
+        httpServer.createContext("/epics", new EpicHandler());
+        httpServer.createContext("/history", new HistoryHandler());
+        httpServer.createContext("/prioritized", new PrioritizedHandler());
+
+
     }
+
+    static class TaskHandler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+
+        }
+
+    }
+
+    static class SubTaskHandler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+
+        }
+
+    }
+
+    static class EpicHandler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+
+        }
+
+    }
+
+    static class HistoryHandler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+
+        }
+
+    }
+
+    static class PrioritizedHandler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+
+        }
+
+    }
+
+
 }
